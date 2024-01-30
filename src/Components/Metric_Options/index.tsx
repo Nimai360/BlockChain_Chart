5369456c7cf562dc02558ab91bbaa4cbd99a9425
Nimai360 <nimai360@hotmail.com>
2024-01-30 10:51:09 -0300

    [feat]: Adicionado mensagem -Search Not Found- nas metricas

--- src/Components/Metric_Options/index.tsx
+++ src/Components/Metric_Options/index.tsx
@@ -56,48 +56,62 @@ const Metric_Options: React.FC<Metric_OptionsProps> = ({
               Show all events{" "}
             </ListItem>
             <div className="p-0 m-0">
-              {allMetrics
-                .filter((item) =>
-                  item.metric_description
-                    .toLowerCase()
-                    .includes(searchText.toLowerCase())
-                )
-                .map((item, index) => { 
-                  return(
-                  <ListItem
-                    placeholder=""
-                    key={index}
-                    className={`m-0 mb-[1px] gap-[10px] py-[10px] px-[4px] font-inter font-medium text-[12px] hover:bg-primary_100 leading-[18px] 
+              {!allMetrics.some((item) =>
+                item.metric_description
+                  .toLowerCase()
+                  .includes(searchText.toLowerCase())
+              ) ? (
+                <p className="text-center my-0 p-0 font-inter text-[16px] text-neutral_600">Event not found</p>
+              ) : (
+                allMetrics
+                  // .filter((item) =>
+                  //   item.metric_description
+                  //     .toLowerCase()
+                  //     .includes(searchText.toLowerCase())
+                  // )
+                  .map((item, index) => {
+                    if (
+                      item.metric_description
+                        .toLowerCase()
+                        .includes(searchText.toLowerCase())
+                    )
+                      return (
+                        <ListItem
+                          placeholder=""
+                          key={index}
+                          className={`m-0 mb-[1px] gap-[10px] py-[10px] px-[4px] font-inter font-medium text-[12px] hover:bg-primary_100 leading-[18px] 
                 ${
                   item.checked
                     ? "bg-primary_500_brand text-white_color"
                     : "text-neutral_800"
                 }`}
-                    onClick={(event) => handleChange(event, item)}
-                  >
-                    <ListItemPrefix placeholder="" className="p-0 m-0">
-                      <svg
-                        width="16"
-                        height="16"
-                        viewBox="0 0 16 16"
-                        className=" size-[16px]"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <g id="cursor-05">
-                          <path
-                            id="Vector"
-                            stroke="currentColor"
-                            fill="transparent"
-                            d="M3.74628 8.29099L2.89418 9.14308M2.80514 6.01845H1.6001M2.89418 2.89438L3.74628 3.74647M6.01881 1.6V2.80504M9.14289 2.89438L8.29079 3.74647M10.6785 10.6636L13.7467 9.5926C14.3218 9.39184 14.3441 8.58653 13.7807 8.36295L6.76101 5.91836C6.23333 5.70894 5.70272 6.22658 5.89887 6.75942L8.19744 13.9746C8.40675 14.5432 9.21166 14.5414 9.4271 13.9718L10.6785 10.6636Z"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                          />
-                        </g>
-                      </svg>
-                    </ListItemPrefix>
-                    {item.metric_description}
-                  </ListItem>
-                )})}
+                          onClick={(event) => handleChange(event, item)}
+                        >
+                          <ListItemPrefix placeholder="" className="p-0 m-0">
+                            <svg
+                              width="16"
+                              height="16"
+                              viewBox="0 0 16 16"
+                              className=" size-[16px]"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <g id="cursor-05">
+                                <path
+                                  id="Vector"
+                                  stroke="currentColor"
+                                  fill="transparent"
+                                  d="M3.74628 8.29099L2.89418 9.14308M2.80514 6.01845H1.6001M2.89418 2.89438L3.74628 3.74647M6.01881 1.6V2.80504M9.14289 2.89438L8.29079 3.74647M10.6785 10.6636L13.7467 9.5926C14.3218 9.39184 14.3441 8.58653 13.7807 8.36295L6.76101 5.91836C6.23333 5.70894 5.70272 6.22658 5.89887 6.75942L8.19744 13.9746C8.40675 14.5432 9.21166 14.5414 9.4271 13.9718L10.6785 10.6636Z"
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                />
+                              </g>
+                            </svg>
+                          </ListItemPrefix>
+                          {item.metric_description}
+                        </ListItem>
+                      );
+                  })
+              )}
             </div>
           </div>
         </List>
